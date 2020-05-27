ddc40f86f7a759f3a177d7865600b9ffe461ca34
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-05-27 17:00:58 +0200

    prepare project packages install dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
   return (
     <div className="App">
       <h2>Ghadan Website</h2>
+      <FontAwesomeIcon icon={faFacebookF} />
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
