ceb6265e821821672cf4d09b91694cbcf2228496
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-07-08 13:08:42 +0200

    add contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,36 @@
 import React, { Fragment } from 'react';
+import './Contact.css'
 
 const Contact = () => {
   return (
     <Fragment>
-      <h1>Contact</h1>
+      <div className='contact-us my-title'>
+        <div className='container'>
+          <div className='title'>
+            <h2>Contact Us</h2>
+            <div className='line'></div>
+          </div>
+          <form>
+            <div className='name'>
+              <input type='text' name='name' id='name' placeholder='Name' />
+              <input type='email' name='email' id='email' placeholder='Email' />
+            </div>
+            <input
+              type='text'
+              name='subject'
+              id='subject'
+              placeholder='Subject'
+            />
+            <textarea
+              name='message'
+              id=''
+              placeholder='Message for me'
+            ></textarea>
+          </form>
+        </div>
+      </div>
     </Fragment>
   );
-}
+};
 
 export default Contact;
